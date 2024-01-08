7a31cf0b549ad1896d069c7f4570be6159cc73b8
TachiYassine <tachi.yassine.3@gmail.com>
2024-01-08 10:07:08 -0800

    Removes characters from string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 def no_c(my_string):
     result = ""
-    for char in my_string:
-        if char != 'c' and char != 'C':
-            result += char
+    for i in range(len(my_string)):
+        if my_string[i] != 'c' and my_string[i] != 'C':
+            result += my_string[i]
     return result
