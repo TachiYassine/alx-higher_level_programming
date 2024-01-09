b94f4a1cf7cc3f09a52785999e4dca4c52e2d14e
TachiYassine <tachi.yassine.3@gmail.com>
2024-01-08 16:22:06 -0800

    Lists of lists = Matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return None
+    for raw in matrix:
+        for element in raw:
+            print(element, end=" ")  # Using end=" " to print elements in the same line
+        print("")  # Printing a new line after each sublist
