18d61ba1be2a756abf0231913d743a919c93e620
TachiYassine <tachi.yassine.3@gmail.com>
2024-06-29 00:28:27 +0100

    Tasks: test-Python-ORM-SQL

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,17 +1,19 @@
-#!/usr/bin/python3
-"""Lists states"""
-
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
-from model_city import City
-
-Base = declarative_base()
-
-
-class State(Base):
-    """Class representing the states table"""
-    __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False,
-                autoincrement=True, unique=True)
-    name = Column(String(128), nullable=False)
+<<<<<<<<<<<<<<  ✨ Codeium Command 🌟 >>>>>>>>>>>>>>>>
+ #!/usr/bin/python3
+ """Lists states"""
+ 
+ from sqlalchemy import Column, Integer, String
+ from sqlalchemy.orm import relationship
+ from sqlalchemy.ext.declarative import declarative_base
++from .model_city import City
+ 
+ Base = declarative_base()
+ 
+ 
+ class State(Base):
+     """Class representing the states table"""
+     __tablename__ = 'states'
++    id = Column(Integer, primary_key=True,
+                 autoincrement=True, unique=True)
++    name = Column(String(128), nullable=False)
+<<<<<<<  8e2ca701-90ae-4a38-9998-ee785a6f9d4c  >>>>>>>
